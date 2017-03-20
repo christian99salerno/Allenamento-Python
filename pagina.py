5c7dd03a7e9e1d2361a14878bcfbe48a1ea9b8de
christian99salerno <christian.salerno05@gmail.com>
2017-03-20 13:56:55 +0100

    pagina completata

--- pagina.py
+++ pagina.py
@@ -4,19 +4,18 @@ class Pagina:
     def __init__(self,art):
         self.art=art
         
-    def setPagina(self,art):
-        self.art=art
-        art=[+self.art]
+    def setPagina(self,art,index_art):
+        self.art[index_art]=art
     
     def getPagina(self):
         return self.art
+
+    def aggiungi_articolo(self,art):
+        self.art.append(art)
     
     def __str__(self):
+        arts=""
         for i in self.art:
-            return str(self.art) 
+            arts=arts+str(i)
+        return arts    
 
-
-art1=Articolo("Hello World!","How are you?")
-art2=Articolo("Ciao Mondo!","Come stai?")
-pag=Pagina(art=[art1,art2])        
-print pag
