def0ec42b208930cc63b40e418d3d9edb8e3c2ab
christian99salerno <christian.salerno05@gmail.com>
2017-03-20 14:02:18 +0100

    giornale da vedere se va bene

--- giornale.py
+++ giornale.py
@@ -3,9 +3,15 @@ from pagina import Pagina
 class Giornale:
     def __init__(self,pag):
         self.pag=pag
-    def setGiornale(self,pag):
-        self.pag=pag
+    def setGiornale(self,pag,index_pag):
+        self.pag[index_pag]=pag
     def getGiornale(self,pag):
         return self.pag
+    def aggiungi_pagina(self,pag):
+        self.pag.append(pag)
     def __str__(self):
+        pags=""
+        for i in self.pag:
+            pags=pags+str(i)
+        return pags
         
